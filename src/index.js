d305ceae82c882335ef6f76f59cbb237425d97f9
Mariia Kovalova <mashuha1698@gmail.com>
2023-01-05 22:59:25 +0200

    simple lghtbox

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import ImagesApiService from './js/images-service';
 import renderCards from './js/render-cards';
 import PageInterface from './js/page-interface';
 import NotifyApi from './js/notify-service';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import 'material-icons/iconfont/material-icons.css';
 import './sass/index.scss';
 
@@ -14,6 +16,10 @@ const refs = {
 const imagesApiService = new ImagesApiService();
 const pageInterface = new PageInterface();
 const notifyApi = new NotifyApi();
+let lightbox = new SimpleLightbox('.gallery .gallery__item', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 pageInterface.hide(refs.loadMoreBtn);
 
@@ -45,6 +51,7 @@ function markup({ data: { hits: imgArr, totalHits: limit } }) {
   }
 
   renderCards(imgArr, refs.gallery);
+  lightbox.refresh();
   imagesApiService.imagesLoadedCount = numberOfImages;
 
   if (imagesApiService.imagesLoadedCount === imagesApiService.perPage) {
--- src/js/render-cards.js
+++ src/js/render-cards.js
@@ -9,30 +9,31 @@ function renderCards(hits, element) {
         views,
         comments,
         downloads,
-      }) =>
-        `<div class="photo-card">
-    <div class="thumb">
-      <img src="${webformatURL}" alt="${alt}" loading="lazy" />
+      }) => `<a class="gallery__item" href="${largeImageURL}">
+    <div class="photo-card">
+      <div class="thumb">
+        <img src="${webformatURL}" alt="${alt}" loading="lazy" />
+      </div>
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${downloads}
+        </p>
+      </div>
     </div>
-    <div class="info">
-      <p class="info-item">
-        <b>Likes</b>
-        ${likes}
-      </p>
-      <p class="info-item">
-        <b>Views</b>
-        ${views}
-      </p>
-      <p class="info-item">
-        <b>Comments</b>
-        ${comments}
-      </p>
-      <p class="info-item">
-        <b>Downloads</b>
-        ${downloads}
-      </p>
-    </div>
-  </div>`
+  </a>`
     )
     .join('');
 
